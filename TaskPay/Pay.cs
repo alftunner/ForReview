129b9115e72e7c80a07f56bdf541da921240d37b
alftunner <alftunner@gmail.com>
2020-08-29 10:42:32 +0300

    first commit

---
+++ TaskPay/Pay.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+namespace TaskPay
+{
+    class Pay
+    {
+        public double payForDay { set; get; } //Оплата за день
+        public int dayCount { set; get; } // количество дней
+        public double fineForDay { set; get; } // штраф за просрочку 1 дня
+        public int dayFine { set; get; } // количество дней с просрочкой
+        public double payWithoutFine { set; get; } // Итого без штрафов
+        public double fine { set; get; } // Общая сумма штрафа
+        public double totalPay { set; get; } // итого к оплате
+        public static bool flag = false;
+
+        public Pay()
+        {
+            
+        }
+
+        public Pay(double payForDay, int dayCount, double fineForDay, int dayFine)
+        {
+            this.payForDay = payForDay;
+            this.dayCount = dayCount;
+            this.fineForDay = fineForDay;
+            this.dayFine = dayFine;
+            payWithoutFine = this.payForDay * this.dayCount;
+            fine = this.fineForDay * this.dayFine;
+            totalPay = payWithoutFine + fine;
+        }
+        
+    }
+}
---
+++ TaskPay/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using static System.Console;
+
+namespace TaskPay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string json = "";
+            Pay.flag = true;
+
+            Pay bill = new Pay(5, 3, 2, 1);
+            serial(Pay.flag, bill,ref json);
+
+            Pay bill2 = new Pay();
+            deSerial(ref json, bill2);
+
+            WriteLine(bill2.dayCount);
+
+
+        }
+
+        public static void writeToFile(string x)
+        {
+            using (FileStream fstream = new FileStream($"class.json", FileMode.OpenOrCreate))
+            {
+                // преобразуем строку в байты
+                byte[] array = System.Text.Encoding.Default.GetBytes(x);
+                // запись массива байтов в файл
+                fstream.Write(array, 0, array.Length);
+            }
+        }
+        public static void serial(bool flag, Pay pay, ref string json)
+        {
+            if (flag)
+            {
+                json = JsonSerializer.Serialize(pay);
+                writeToFile(json);
+                WriteLine("Объект сериализован и записан в фаил");
+            }
+            else
+            {
+                WriteLine("Объект не нужно сериализовать");
+            }
+        }
+
+        public static void deSerial(ref string json, Pay pay)
+        {
+           pay = JsonSerializer.Deserialize<Pay>(json);
+            WriteLine("Объект десиарилизован и создан объект bill2");
+        }
+    }
+}
